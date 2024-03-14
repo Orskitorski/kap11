d06973cfb9bd000503fcaedbf4306584a7c359ec
Orskitorski <oscar.tafvelin@elev.ga.ntig.se>
2024-03-14 10:31:20 +0100

    skrev fler kommentarer

--- src/upg111.java
+++ src/upg111.java
@@ -6,7 +6,7 @@ public class upg111 {
         int [] numberArray = new int[1000]; //skapar en ny array med storleken 1000
         int length = 0; //skapar en int för att hålla koll på längden senare
         for (int i = 0; i < 1000; i++){ //for loop som läser in användarens svar
-            if (tgb.hasNextInt()) {
+            if (tgb.hasNextInt()) { //avslutar så fort användaren skriver in någonting annat förutom en int
                 numberArray[i] = tgb.nextInt(); //sätter varje position i arrayen till ett värde användaren skrivit in
                 length++;
             }
@@ -19,16 +19,15 @@ public class upg111 {
                     secondArrayPosition++;
                 }
                 if (numberArray[secondArrayPosition] == numberArray[firstArrayPosition]) {
-                    for(int j = secondArrayPosition; j < length; j++)
-                    {
-                        numberArray[j] = numberArray[j+1]; //ersätter kopian och flyttar fram alla tal efter det ett steg
+                    for(int j = secondArrayPosition; j < length; j++) {
+                        numberArray[j] = numberArray[j+1]; //ersätter kopian och flyttar fram alla tal efter kopian ett steg
                     }
-                    length--;
+                    length--; //längden på utskriften minskar varje gång man tar bort ett tal
                 }
             }
         }
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length; i++) { //for loop som skriver ut den färdiga arrayen, begränsat till längden
             System.out.println(numberArray[i]);
         }
     }
